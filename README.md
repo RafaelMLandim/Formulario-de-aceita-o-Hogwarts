0ed7e0987e917a90f85b9a9c47a62333b6d6ac02
RafaelMlandim <rafaelmlandim@hotmail.com>
2024-10-16 23:14:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+🧙‍♂️✨ Hogwarts House Sorting Quiz ✨🏰
+🎩🧙‍♀️ Bem-vindo ao Hogwarts House Sorting Quiz! Prepare-se para descobrir qual casa mágica de Hogwarts você pertence: Grifinória, Sonserina, Corvinal ou Lufa-Lufa! 🌟 Com um toque de mistério, magia e aventura, esse quiz vai te guiar até sua verdadeira casa em Hogwarts. 🏰✨
+
+⚡ Funcionalidades Mágicas ⚡
+📜 Formulário Inicial Encantado: Comece inserindo seu nome e sobrenome mágicos. 🌟 Depois, nos diga: seus pais são bruxos ou trouxas? 🔮✨
+
+💡 Perguntas Dinâmicas e Místicas: Responda a perguntas que mudam de acordo com cada casa de Hogwarts! As perguntas foram projetadas para revelar sua verdadeira essência mágica. 💫
+
+🎯 Sistema de Pontuação Feitiçado:
+
+Sim 🟢 = 3 pontos 🧙‍♂️
+Talvez 🟡 = 1 ponto ✨
+Não 🔴 = 0 pontos ❌
+🏆 Revelação da Casa: Após responder todas as perguntas, você descobrirá a casa com maior pontuação! O sistema mostra um belo parágrafo descritivo junto com o logotipo da casa vencedora e altera o fundo mágico da página para combinar com suas cores. 🏰💫
+
+🎨 Como funciona a magia?
+Escolha seu destino: Ao responder as perguntas, sua pontuação é registrada para cada casa de Hogwarts.
+Feitiço de cálculo: No final, somamos tudo com nossa fórmula mágica:
+Sim = +3 pontos 💥
+Talvez = +1 ponto 🌀
+Não = 0 pontos ⚡
+Sua Casa de Hogwarts: A casa com maior pontuação é revelada com todo o glamour e magia! ✨✨
+🛠️ Tecnologias Mágicas
+HTML 🏗️: Para estruturar a magia da página.
+CSS 🎨: Para dar aquele estilo e visual fantástico.
+JavaScript 🔮: O cérebro por trás da lógica mágica, incluindo a exibição das perguntas e cálculo da pontuação.
+LocalStorage 📦: Para guardar os dados de quem já fez o quiz.
+🏰 Como rodar o quiz?
+Clone este repositório. 🧙‍♀️
+Abra o arquivo index.html no seu navegador favorito. 🌐
+Preencha o formulário e responda às perguntas. ⚡
+Descubra sua casa de Hogwarts e compartilhe com seus amigos bruxos! 🧙‍♂️
+🌈 Imagens de Hogwarts ✨
+Cada casa é representada por uma imagem incrível:
+
+🦁 Grifinória: Coragem e bravura!
+🐍 Sonserina: Astúcia e ambição!
+🦅 Corvinal: Sabedoria e criatividade!
+🦡 Lufa-Lufa: Lealdade e trabalho duro!
+
+Que a magia te guie! 🔮
---
+++ index.js
@@ -0,0 +1,197 @@
+const perguntasDeHogwarts = [
+  // Grifinória
+  {
+    casa: 'Grifinória',
+    pergunta: 'Em uma situação difícil, você enfrenta o problema de frente ou procura uma solução alternativa?'
+  },
+  {
+    casa: 'Grifinória',
+    pergunta: 'Você prefere liderar seus amigos em aventuras arriscadas ou seguir em busca de justiça, mesmo que isso exija sacrifícios?'
+  },
+  // Sonserina
+  {
+    casa: 'Sonserina',
+    pergunta: 'Você prefere traçar um plano estratégico para vencer ou usar seu carisma para influenciar os outros?'
+  },
+  {
+    casa: 'Sonserina',
+    pergunta: 'Se fosse necessário, você trairia alguém para garantir seu sucesso ou encontraria uma maneira de se beneficiar sem prejudicar os outros?'
+  },
+  // Corvinal
+  {
+    casa: 'Corvinal',
+    pergunta: 'Você gosta de resolver problemas complexos usando lógica e raciocínio ou prefere buscar conhecimento por meio da exploração criativa?'
+  },
+  {
+    casa: 'Corvinal',
+    pergunta: 'Você se sentiria mais satisfeito em ser reconhecido pela sua inteligência ou pela sua capacidade de pensar de forma inovadora?'
+  },
+  // Lufa-Lufa
+  {
+    casa: 'Lufa-Lufa',
+    pergunta: 'Você valoriza mais a honestidade em suas relações ou a lealdade para com aqueles próximos a você?'
+  },
+  {
+    casa: 'Lufa-Lufa',
+    pergunta: 'Você prefere trabalhar duro para alcançar seus objetivos ou ser paciente e esperar pelo momento certo para agir?'
+  }
+];
+
+// Variável para rastrear a pergunta atual
+let perguntaAtual = 0;
+let pontuacaoPorCasa;
+// Obter respostas armazenadas do localStorage ou inicializar um novo array
+let respostas = JSON.parse(localStorage.getItem('respostasFormulario')) || [];
+
+// Adiciona um listener para o evento de submit do formulário
+document.getElementById('meuForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let nome = document.getElementById('nome').value;
+  let sobrenome = document.getElementById('sobrenome').value;
+
+  let dados = {
+    nome: nome,
+    sobrenome: sobrenome
+  };
+
+  // Armazena os dados do formulário no localStorage
+  localStorage.setItem('dadosFormulario', JSON.stringify(dados));
+
+  // Esconder o formulário e mostrar o questionário
+  document.getElementById('meuformulario').classList.add('hidden');
+  mostrarPergunta();
+});
+
+// Função para mostrar a pergunta na divPergunta
+function mostrarPergunta() {
+  if (perguntaAtual < perguntasDeHogwarts.length) {
+    const divPergunta = document.getElementById("divPergunta");
+    divPergunta.innerHTML = ''; // Limpa o conteúdo anterior
+
+    // Cria o elemento da pergunta
+    const perguntaElement = document.createElement("p");
+    perguntaElement.innerText = perguntasDeHogwarts[perguntaAtual].pergunta;
+    divPergunta.appendChild(perguntaElement);
+
+    // Adiciona as opções de resposta
+    const opcoes = ['sim', 'talvez', 'nao'];
+    opcoes.forEach(opcao => {
+      const label = document.createElement("label");
+      label.innerHTML = `<input type="radio" name="resposta" value="${opcao}" required> 
+      ${opcao.charAt(0).toUpperCase() + opcao.slice(1)} `;
+      divPergunta.appendChild(label);
+    });
+
+    // Botão continuar
+    const continuarButton = document.createElement("button");
+    continuarButton.type = "button";
+    continuarButton.innerText = "Continuar";
+    continuarButton.onclick = continuar; // Chama a função continuar ao clicar
+    divPergunta.appendChild(continuarButton);
+
+    // Mostra a div com a pergunta
+    divPergunta.style.display = 'flex';
+    divPergunta.style.flexDirection = 'column';
+  } else {
+    // Aqui você pode armazenar ou processar as respostas
+    localStorage.setItem('respostasFormulario', JSON.stringify(respostas));
+    const resultado = JSON.parse(localStorage.getItem('respostasFormulario'))
+
+    pontuacaoPorCasa = calcularPontuacao(resultado);
+    resultadoFinal()
+  }
+}
+const calcularPontuacao = (respostas) => {
+  // Define a pontuação para cada resposta
+  const pontos = {
+    "sim": 3,
+    "nao": 0,
+    "talvez": 1
+  };
+
+  // Usa reduce para acumular os pontos por casa
+  return respostas.reduce((acumulador, item) => {
+    const casa = item.casa;
+    const resposta = item.resposta;
+
+    // Se a casa ainda não existe no acumulador, inicializa com 0
+    if (!acumulador[casa]) {
+      acumulador[casa] = 0;
+    }
+
+    // Soma os pontos da resposta correspondente à casa
+    acumulador[casa] += pontos[resposta];
+
+    return acumulador;
+  }, {}); // O {} inicializa o acumulador como um objeto vazio
+};
+
+// Função chamada ao clicar no botão "Continuar"
+function continuar() {
+  // Captura a resposta atual
+  const respostaSelecionada = document.querySelector('input[name="resposta"]:checked');
+  if (respostaSelecionada) {
+    // Armazena a resposta
+    respostas.push({
+      pergunta: perguntasDeHogwarts[perguntaAtual].pergunta,
+      resposta: respostaSelecionada.value,
+      casa: perguntasDeHogwarts[perguntaAtual].casa
+    });
+
+    perguntaAtual++; // Avança para a próxima pergunta
+    mostrarPergunta(); // Mostra a próxima pergunta
+  } else {
+    alert('Por favor, selecione uma resposta antes de continuar.');
+  }
+}
+
+function resultadoFinal() {
+
+  document.querySelector('#tudo').style.display = 'none';
+  const usuario = JSON.parse(localStorage.getItem('dadosFormulario'))
+  const casaSelecionada = document.querySelector('#casaSelecionada');
+
+
+
+  const casaComMaiorPontuacao = Object.entries(pontuacaoPorCasa).reduce((acumulador, atual) => {
+    return atual[1] > acumulador[1] ? atual : acumulador;
+  }, ["", 0]);
+
+  const casaVencedora = casaComMaiorPontuacao[0];
+
+  // Limpa o conteúdo anterior
+  casaSelecionada.innerHTML = "";
+
+  // Modifica o conteúdo da div e o fundo do body para a casa correspondente
+  if (casaVencedora === 'Grifinória') {
+    casaSelecionada.innerHTML = `
+      <p>Parabéns ${usuario.nome}! Você pertence à Grifinória!</p>
+      <img src="./img/grifinoria.png" alt="Logo Grifinória">
+    `;
+    document.body.style.backgroundImage = "url('img/grif.jpeg')"; // Cor de fundo para Grifinória
+  }
+  if (casaVencedora === 'Sonserina') {
+    casaSelecionada.innerHTML = `
+      <p>Parabéns ${usuario.nome}! Você pertence à Sonserina!</p>
+      <img src="./img/sonserina.png" alt="Logo Sonserina">
+    `;
+    document.body.style.backgroundImage = "url('img/sons.jpeg')"; // Cor de fundo para Sonserina
+  }
+  if (casaVencedora === 'Corvinal') {
+    casaSelecionada.innerHTML = `
+      <p>Parabéns ${usuario.nome}! Você pertence à Corvinal!</p>
+      <img src="./img/corvinal.png" alt="Logo Corvinal">
+    `;
+    document.body.style.backgroundImage = "url('img/corvi.jpeg')"; // Cor de fundo para Corvinal
+  }
+  if (casaVencedora === 'Lufa-Lufa') {
+    casaSelecionada.innerHTML = `
+      <p>Parabéns ${usuario.nome}! Você pertence à Lufa-Lufa!</p>
+      <img src="./img/lufa-lufa.png" alt="Logo Lufa-Lufa">
+    `;
+    document.body.style.backgroundImage = "url('img/lufa.jpeg')"; // Cor de fundo para Lufa-Lufa
+  }
+  localStorage.clear()
+}
+
